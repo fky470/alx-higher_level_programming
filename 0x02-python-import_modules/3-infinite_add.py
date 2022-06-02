568c2055d6eac4e65438c25fd93f20382d999cdf
fky470 <fikreyohannesbiruk@gmail.com>
2022-06-02 20:49:13 +0300

    adding file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_args(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        sum = 0
+        while i <= n:
+            sum += int(argv[i])
+            i += 1
+        print("{:d}".format(sum))
+if __name__ == "__main__":
+    import sys
+    add_args(sys.argv)
