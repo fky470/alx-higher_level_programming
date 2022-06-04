ea653913193dc2cf3c80034bca1b0cee467a3fab
fky470 <fikreyohannesbiruk@gmail.com>
2022-06-04 23:06:40 +0300

    adding file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
+    first_char = sentence[0]
     if length > 0:
-        tup = length, sentence[0]
+        tup = length, first_char
         return tup
     else:
         None
