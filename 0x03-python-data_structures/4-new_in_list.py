715d71b5d9e385dd96d8645d4cf817a79f241b4c
fky470 <fikreyohannesbiruk@gmail.com>
2022-06-04 19:14:05 +0300

    adding 4-new_in_list.py file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     tmp_list = my_list[:]
     if (idx >= 0) and (len(my_list)):
         tmp_list[idx] = element
-        return tmp_list
+    return tmp_list
+    
     return my_list
